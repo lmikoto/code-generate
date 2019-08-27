c1099e877a57a7bc9c5b0a96721bac9a7a6bb0f1
lmikoto <lmikoto@outlook.com>
2019-08-28 00:12:11 +0800

    init

--- src/main/java/io/github/lmikoto/action/CreateFile.java
+++ src/main/java/io/github/lmikoto/action/CreateFile.java
@@ -5,6 +5,7 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiUtilBase;
 import io.github.lmikoto.gui.CreateFileDialog;
 
 public class CreateFile extends AnAction {
@@ -15,15 +16,17 @@ public class CreateFile extends AnAction {
         CreateFileDialog createFile = new CreateFileDialog();
         createFile.pack();
         createFile.setVisible(true);
-
-        PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
-
         Project project = e.getProject();
 
-        DataContext dataContext = e.getDataContext();
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
+        PsiFile psiFile = PsiUtilBase.getPsiFileInEditor(editor, project);
 
-        Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
-        PsiFile fiuke = CommonDataKeys.PSI_FILE.getData(dataContext);
-        VirtualFile virtualFile = CommonDataKeys.VIRTUAL_FILE.getData(dataContext);
+
+//
+//        DataContext dataContext = e.getDataContext();
+//
+//        Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
+//        PsiFile fiuke = CommonDataKeys.PSI_FILE.getData(dataContext);
+//        VirtualFile virtualFile = CommonDataKeys.VIRTUAL_FILE.getData(dataContext);
     }
 }
